1c7fd677c146330b42a9ec8fc0e24616e2bc1b30
Larry <larryjamestaylor@gmail.com>
2017-07-21 09:01:20 -0700

    set up index.php, install composer, add inital app skeleton

---
+++ app/app.php
@@ -0,0 +1,18 @@
+<?php
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Store.php";
+
+    $server = 'mysql:host=localhost:8889;dbname=shoes';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    $app = new Silex\Application();
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+
+    // routes
+
+    return $app;
