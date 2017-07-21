bb231be7be33a411138345ed3d80917e49952114
Larry <larryjamestaylor@gmail.com>
2017-07-21 16:11:37 -0700

    add route to return to inde and delete all stores

--- app/app.php
+++ app/app.php
@@ -53,5 +53,10 @@
         return $app['twig']->render('stores.html.twig', array('stores' => Store::getAll()));
     });
 
+    $app->post("/delete_stores", function() use($app) {
+        Store::deleteAll();
+        return $app['twig']->render('index.html.twig');
+    });
+
     return $app;
 ?>
