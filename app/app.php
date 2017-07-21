9ba19b935f2f774cbd649f4561c466a26a41c6ff
Larry <larryjamestaylor@gmail.com>
2017-07-21 16:09:03 -0700

    add stores.html.twig with route in app

--- app/app.php
+++ app/app.php
@@ -39,5 +39,10 @@
         return $app['twig']->render('index.html.twig');
     });
 
+/////////////////////////////////////////
+
+    $app->get('/stores', function() use($app) {
+        return $app['twig']->render('stores.html.twig', array('stores' => Store::getAll()));
+    });
     return $app;
 ?>
